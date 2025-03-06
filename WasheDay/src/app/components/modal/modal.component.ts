2e0f34cd9f8a8850d74e030ec37f474419436aa9
hector <cervaya.003@gmail.com>
2025-03-06 16:41:47 -0600

    view washo

---
+++ WasheDay/src/app/components/modal/modal.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+import { ModalController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.scss'],
+  standalone: true, // Si usando componentes independientes (standalone)
+  imports: [IonicModule, CommonModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA], //esquema personalizado
+})
+export class ModalComponent implements OnInit {
+  constructor(
+    private menuCtrl: MenuController,
+    private router: Router,
+    private modalCtrl: ModalController
+  ) {}
+
+  ngOnInit() {}
+  
+  cerrarModal() {
+    this.modalCtrl.dismiss();
+  }
+}
