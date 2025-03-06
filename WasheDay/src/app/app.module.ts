0dbc73c781be91d7723c4d3662ef6d73f653d103
hector <cervaya.003@gmail.com>
2025-03-06 16:41:47 -0600

    view washo

--- WasheDay/src/app/app.module.ts
+++ WasheDay/src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
+import { GoogleMapsModule } from '@angular/google-maps'; // Cambia esto
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    GoogleMapsModule // Aquí se importa el módulo correcto
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
