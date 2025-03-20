3db72165b4408b53975a18371a3008a5b129c4f2
hector <cervaya.003@gmail.com>
2025-03-19 18:53:10 -0600

    nav bar and search with IA

---
+++ WasheDay/src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  apiUrl: {
+    web: "AIzaSyDTXVkRugwfDPQijwpw7MD0W8IkjpjI-NU",
+    android: "AIzaSyDxUWohVZ8KfnxOsajryWdi3oVLfBpQjts",
+    ios: ""
+  },
+  apiUrl_IA: '',
+};
