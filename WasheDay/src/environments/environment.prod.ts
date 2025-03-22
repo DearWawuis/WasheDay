53fd39b4bf0beb1b12397bfc7d7f1c8f3e558420
hector <cervaya.003@gmail.com>
2025-03-21 19:15:58 -0600

    actualización

--- WasheDay/src/environments/environment.prod.ts
+++ WasheDay/src/environments/environment.prod.ts
@@ -5,5 +5,5 @@ export const environment = {
     android: "AIzaSyDxUWohVZ8KfnxOsajryWdi3oVLfBpQjts",
     ios: ""
   },
-  apiUrl_IA: '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',
+  apiUrl_IA: '',
 };
