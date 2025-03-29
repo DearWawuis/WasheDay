6bcd7ea664bca594297de333c911d9229e14ebfd
Nino2502 <ninoleal2469@gmail.com>
2025-03-28 21:32:00 -0600

    Se cambio las env

--- WasheDay/src/environments/environment.prod.ts
+++ WasheDay/src/environments/environment.prod.ts
@@ -1,9 +1,11 @@
 export const environment = {
   production: true,
+  stripe_pk: 'pk_test_51R2TH0QNgl65mkjdhPW7iVGpwkIT6BCSrPXJ4IfC5dKTdPt6m8F26Daq281x6wsCIdHbYcjYy5mOB9J4XsohgTmx00gEzFhd9n',
   apiUrl: {
     web: "AIzaSyDTXVkRugwfDPQijwpw7MD0W8IkjpjI-NU",
     android: "AIzaSyDxUWohVZ8KfnxOsajryWdi3oVLfBpQjts",
     ios: ""
   },
-  apiUrl_IA: '',
+  apiUrl_IA: '', // Puede mantenerse vacío o agregar un endpoint adicional si es necesario
+  api: 'https://api-washeday.vercel.app'  // La URL de tu backend en producción
 };
